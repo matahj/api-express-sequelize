270676e45322605a33cf50d750d92d5ea800fd4f
JorgeMataH <matahj@gmail.com>
2024-07-12 12:01:30 -0600

    Se agregaron algunas operaciones CRUD

--- index.js
+++ index.js
@@ -4,129 +4,128 @@ require("dotenv").config();
 const { Sequelize, DataTypes, json } = require("sequelize");
 
 const sequelize = new Sequelize({
-    dialect: "mysql",
-    host: process.env.MYSQL_HOST,
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+  dialect: "mysql",
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 async function connect() {
-    try {
-        await sequelize.authenticate();
-        console.log("Conectado a la BD.")
-    } catch (e) {
-        console.error("No se puede conectar a la BD.");
-        console.error(e);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Conectado a la BD.");
+  } catch (e) {
+    console.error("No se puede conectar a la BD.");
+    console.error(e);
+  }
 }
 
 connect();
 
-const Terminal = sequelize.define("terminales",
-    {
-        nombre: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        },
-        estado: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        },
-        direccion: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        }
+const Terminal = sequelize.define(
+  "terminales",
+  {
+    nombre: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
     },
-    {
-        timestamps: false
-    }
-);
-
-const Autobus = sequelize.define("autobuses",
-    {
-        matricula: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-            unique: true
-        },
-        modelo: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-        },
-        anio: {
-            type: DataTypes.STRING(10),
-            allowNull: false,
-        },
-        activo: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        }
+    estado: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
     },
-    {
-        timestamps: false
-    }
+    direccion: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
 );
 
-const Viaje = sequelize.define("viajes",
-    {
-        asientosDisponibles: {
-            field: "asientos_disponibles",
-            type: DataTypes.INTEGER,
-            validate: { min: 0, max: 40 },
-            allowNull: false,
-            defaultValue: 40
-        },
-        fechaSalida: {
-            field: "fecha_salida",
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        precio: {
-            type: DataTypes.DOUBLE,
-            validate: { min: 0.0, max: 5000.0 },
-            allowNull: false,
-        }
-    }
+const Autobus = sequelize.define(
+  "autobuses",
+  {
+    matricula: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+      unique: true,
+    },
+    marca: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    anio: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    activo: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
 );
 
-const Administrador = sequelize.define("admins",
-    {
-        nombre: {
-            type: DataTypes.STRING(50),
-            allowNull: false
-        },
-        correo: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            unique: true
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        }
-    }
-);
+const Viaje = sequelize.define("viajes", {
+  asientosDisponibles: {
+    field: "asientos_disponibles",
+    type: DataTypes.INTEGER,
+    validate: { min: 0, max: 40 },
+    allowNull: false,
+    defaultValue: 40,
+  },
+  fechaSalida: {
+    field: "fecha_salida",
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  precio: {
+    type: DataTypes.DOUBLE,
+    validate: { min: 0.0, max: 5000.0 },
+    allowNull: false,
+  },
+});
+
+const Administrador = sequelize.define("admins", {
+  nombre: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  correo: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
 
 Terminal.hasMany(Autobus);
 Autobus.belongsTo(Terminal);
 
 Viaje.belongsToMany(Administrador, {
-    through: "viajes_admins"
+  through: "viajes_admins",
 });
 Administrador.belongsToMany(Viaje, {
-    through: "viajes_admins"
+  through: "viajes_admins",
 });
 
-async function sync(){
-    try{
-        await sequelize.sync();
-        //await sequelize.sync({force: true});
-        console.log("Base de datoa inicializada.");
-    } catch(e){
-        console.error("La BD no se pude actualizar.");
-        console.error(e);
-    }
+async function sync() {
+  try {
+    await sequelize.sync();
+    //await sequelize.sync({force: true});
+    console.log("Base de datoa inicializada.");
+  } catch (e) {
+    console.error("La BD no se pude actualizar.");
+    console.error(e);
+  }
 }
 
 sync();
@@ -137,15 +136,56 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/hola", function(req, res){
-    res.send("Hola desde Express");
+app.get("/hola", function (req, res) {
+  res.send("Hola desde Express");
 });
 
-app.get("/goodbye", function(req, res){
-    res.send("Goodbye desde Express");
+app.get("/goodbye", function (req, res) {
+  res.send("Goodbye desde Express");
 });
 
-app.listen(process.env.PORT_SERVER, function(){
-    console.log("Servidor en el puerto " + process.env.PORT_SERVER);
+////////////////////////////////////////////////
+app.post("/terminal", async function (req, res) {
+  const { nombre, estado, direccion } = req.body;
+
+  const documento = await Terminal.create({ nombre, estado, direccion });
+
+  res.send(documento);
 });
 
+app.get("/terminal", async function (req, res) {
+  documentos = await Terminal.findAll();
+  res.send(documentos);
+});
+
+app.post("/autobus", async function (req, res) {
+  const { matricula, marca, anio, activo, terminaleId } = req.body;
+
+  const documento = await Autobus.create({
+    matricula,
+    marca,
+    anio,
+    activo,
+    terminaleId,
+  });
+
+  res.json(documento);
+});
+
+app.get("/autobus", async function (req, res) {
+  documentos = await Autobus.findAll({ include: Terminal });
+  res.send(documentos);
+});
+
+app.get("/autobus/:id", async function (req, res) {
+  const {id} = req.params;
+  console.log(id);
+  documento = await Autobus.findByPk(id, { include: Terminal });
+  res.send(documento);
+});
+
+////////////////////////////////////////////////
+
+app.listen(process.env.PORT_SERVER, function () {
+  console.log("Servidor en el puerto " + process.env.PORT_SERVER);
+});
